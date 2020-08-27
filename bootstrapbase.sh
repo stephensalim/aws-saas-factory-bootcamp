786e905d0d3fb9538359e10afd5755e8b4983ffe
EC2 Default User <ec2-user@ip-172-31-2-237.ec2.internal>
2020-08-27 23:17:00 +0000

    Add Bootstrap Script, and Cleanup Script

--- bootstrapbase.sh
+++ bootstrapbase.sh
@@ -4,29 +4,32 @@ echo "#######################################"
 echo "# Executing baseline bootstrap script #"
 echo "#######################################"
 
-echo "------------------"
-echo "Creating S3 Bucket"
-echo "------------------"
+echo "--------------------------"
+echo "Step 1. Creating S3 Bucket"
+echo "--------------------------"
 aws s3 mb s3://$1
 
-echo "----------------------"
-echo "Copying bootcamp files"
-echo "----------------------"
+echo "-----------------------------"
+echo "Step2. Copying bootcamp files"
+echo "-----------------------------"
 aws s3 cp --recursive Base/ s3://$1/bootcamp/Base
 aws s3 cp --recursive Lab1/ s3://$1/bootcamp/Lab1
 aws s3 cp --recursive Lab2/ s3://$1/bootcamp/Lab2
 aws s3 cp --recursive Lab3/ s3://$1/bootcamp/Lab3
 
 
-echo "------------------------"
-echo "Executing Baseline Stack"
-echo "------------------------"
+echo "-------------------------------"
+echo "Step3. Executing Baseline Stack"
+echo "-------------------------------"
 aws cloudformation create-stack --stack-name aws-saas-factory-bootcamp \
                                 --template-body file://templates/saas-bootcamp-baseline-master.template \
                                 --capabilities CAPABILITY_NAMED_IAM  CAPABILITY_IAM \
                                 --disable-rollback \
                                 --parameters "ParameterKey"="QSS3BucketName","ParameterValue"=$1 "ParameterKey"="QSS3KeyPrefix","ParameterValue"="bootcamp/" --region $2
 
+echo "---------------------------------"
+echo "Step4. Wait for Stack to Complete"
+echo "---------------------------------"
 STATUS=$(aws cloudformation describe-stacks --region $2 --stack-name aws-saas-factory-bootcamp --query 'Stacks[0].StackStatus')
 count=0
 while [ $STATUS == "CREATE_IN_PROGRESS" ]
--- cleanup.sh
+++ cleanup.sh
@@ -4,9 +4,26 @@ echo "############################"
 echo "# Executing cleanup script #"
 echo "############################"
 
+echo "----------------------------------------------------------"
+echo "Step 1. Delete ProductManagerService CloudFormation stack "
+echo "----------------------------------------------------------"
+
+echo "--------------------------------"
+echo "Step 1.1 Deal with dependancies "
+echo "--------------------------------"
+
+
+echo "----------------------------------------------------------"
+echo "Step 2. Delete ECR Repo created in this stack             "
+echo "----------------------------------------------------------"
+
+echo "----------------------------------------------------------"
+echo "Step 3. Delete S3 All bucket                              "
+echo "----------------------------------------------------------"
 echo "------------------"
 echo "Deleting S3 Bucket with aws-saas-factory-bootcamp prefix in your region"
 echo "------------------"
+
 
 for bucket in $(aws s3 ls | awk '{print $3}' | grep aws-saas-factory-bootcamp)
 do 
@@ -16,5 +33,10 @@ do
     aws s3 rb --force s3://${bucket}
 done
 
+echo "--------------------------------------------------"
+echo "Step 4. Delete Parent Stack                       "
+echo "--------------------------------------------------"
 
-
+echo "--------------------------------"
+echo "Step 4.1 Deal with dependancies "
+echo "--------------------------------"
