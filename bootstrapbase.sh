c5d0b3c96f8b1dbb30023fef67661e8542619714
EC2 Default User <ec2-user@ip-172-31-2-237.ec2.internal>
2020-08-27 12:48:22 +0000

    Update web-client-build.template with gem command

--- bootstrapbase.sh
+++ bootstrapbase.sh
@@ -4,14 +4,39 @@ echo "#######################################"
 echo "# Executing baseline bootstrap script #"
 echo "#######################################"
 
+echo "------------------"
 echo "Creating S3 Bucket"
+echo "------------------"
 aws s3 mb s3://$1
 
-echo "Copying bootcamp Base files"
+echo "----------------------"
+echo "Copying bootcamp files"
+echo "----------------------"
 aws s3 cp --recursive Base/ s3://$1/bootcamp/Base
+aws s3 cp --recursive Lab1/ s3://$1/bootcamp/Lab1
+aws s3 cp --recursive Lab2/ s3://$1/bootcamp/Lab2
+aws s3 cp --recursive Lab3/ s3://$1/bootcamp/Lab3
 
+
+echo "------------------------"
+echo "Executing Baseline Stack"
+echo "------------------------"
 aws cloudformation create-stack --stack-name aws-saas-factory-bootcamp \
                                 --template-body file://templates/saas-bootcamp-baseline-master.template \
                                 --capabilities CAPABILITY_NAMED_IAM  CAPABILITY_IAM \
                                 --disable-rollback \
                                 --parameters "ParameterKey"="QSS3BucketName","ParameterValue"=$1 "ParameterKey"="QSS3KeyPrefix","ParameterValue"="bootcamp/" --region $2
+
+STATUS=$(aws cloudformation describe-stacks --region $2 --stack-name aws-saas-factory-bootcamp --query 'Stacks[0].StackStatus')
+count=0
+while [ $STATUS == '"CREATE_IN_PROGRESS"' ]
+do
+    ((count=count+1))
+
+    echo ">> Checking Status every 5 mins #$count <<"
+    aws cloudformation describe-stack-events --region $2 \
+                                         --stack-name aws-saas-factory-bootcamp \
+                                         --output text \
+                                         --query 'StackEvents[*].[ResourceStatus,LogicalResourceId,ResourceType,Timestamp]'
+    sleep 60
+done
